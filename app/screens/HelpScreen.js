ed5f97752f76e7e7ea1672e650cd6c4a7cfe3ef0
Daan <daan.vanmaldegem@student.hu.nl>
2023-04-05 14:53:28 +0200

    Added a Help section for explaining some parts of the game

--- app/screens/HelpScreen.js
+++ app/screens/HelpScreen.js
@@ -11,39 +11,54 @@ class HelpScreen extends Component {
     }
 
     render() {
-        return(
+        return( // Scrollview.persistentScrollbar is an Android only feature
             <View style={styles.background}>
                 <ForceMode mode={ScreenOrientation.OrientationLock.PORTRAIT}/>
                 <StatusBar backgroundColor={colors.Primary}/>
                 <View style={styles.container}>
-                    <ScrollView contentContainerStyle={styles.scroller}>
+                    <ScrollView contentContainerStyle={styles.scroller} persistentScrollbar={true}>
+                        <View style={styles.howToPlay}>
+                            <Text style={styles.title}>
+                                How To Play:
+                            </Text>
+                            <Text style={styles.normalText}>
+                                1. Enter all the names of the people who want to play along up to a maximum of 10. The more the merrier!
+                            </Text>
+                            <Text style={styles.normalText}>
+                                2. Select all of your prefered categories. The game will only draw prompts from those category pools!
+                            </Text>
+                            <Text style={styles.normalText}>
+                                3. Play the game!
+                            </Text>
+                            <Text style={[styles.normalText, styles.indentedText]}>
+                                Round 1: The person holding the phone reads the prompt out loud. During the first round, people will have to drink
+                                the specified amount of sips if they have (or have not) done the thing specified in the prompt.
+                            </Text>
+                            <Image source={require('../assets/images/round-1-example.png')} style={styles.image}></Image>
+                            <Text style={[styles.normalText, styles.indentedText]}>
+                                Round 2: During the second round the game has changed. This time you can hand out the sips to anyone that is playing the game with you!
+                            </Text>
+                            <Image source={require('../assets/images/round-2-example.png')} style={styles.image}></Image>
+                            <Text style={[styles.normalText, styles.indentedText]}>
+                                Round 3: This is the knockout round. The game will direct a prompt to a specific person (in order of the names entered at the beginning
+                                of the game). If they meet the criteria: Great! they can continue playing. If not: They have to down their drink and are eliminated
+                                from the game. Keep playing until there is only one (or no) winner left!
+                            </Text>
+                            <Image source={require('../assets/images/round-3-example.png')} style={styles.image}></Image>
+                        </View>
                         <Text style={styles.title}>
-                            How To Play:
+                            Help
                         </Text>
+                        <Text style={[styles.normalText, styles.topBorder]}>
+                            What does it mean to 'fold a box'? It's simple: if you have to fold a box, it means you have to down your drink.
+                        </Text>
+                        <Text style={[styles.normalText, styles.topBorder]}>
+                            What happens when a person is included in a question like here?
+                        </Text>
+                        <Image source={require('../assets/images/player-named-example.png')} style={styles.image}></Image>
                         <Text style={styles.normalText}>
-                            1. Enter all the names of the people who want to play along up to a maximum of 10. The more the merrier!
+                            In the event where a player is named, they themselves are also included. In this particular example, Jill also has to drink 2 sips.
                         </Text>
-                        <Text style={styles.normalText}>
-                            2. Select all of your prefered categories. The game will only draw prompts from those category pools!
-                        </Text>
-                        <Text style={styles.normalText}>
-                            3. Play the game!
-                        </Text>
-                        <Text style={[styles.normalText, styles.indentedText]}>
-                            Round 1: The person holding the phone reads the prompt out loud. During the first round, people will have to drink
-                            the specified amount of sips if they have (or have not) done the thing specified in the prompt.
-                        </Text>
-                        <Image source={require('../assets/images/round-1-example.png')} style={styles.image}></Image>
-                        <Text style={[styles.normalText, styles.indentedText]}>
-                            Round 2: During the second round the game has changed. This time you can hand out the sips to anyone that is playing the game with you!
-                        </Text>
-                        <Image source={require('../assets/images/round-2-example.png')} style={styles.image}></Image>
-                        <Text style={[styles.normalText, styles.indentedText]}>
-                            Round 3: This is the knockout round. The game will direct a prompt to a specific person (in order of the names entered at the beginning
-                            of the game). If they meet the criteria: Great! they can continue playing. If not: They have to down their drink and are eliminated
-                            from the game. Keep playing until there is only one (or no) winner left!
-                        </Text>
-                        <Image source={require('../assets/images/round-3-example.png')} style={styles.image}></Image>
                     </ScrollView>
                 </View>
                 <View style={styles.buttonContainer}>
@@ -71,10 +86,15 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         margin: 20,
-        padding: 20,
+        padding: 5,
         backgroundColor: colors.Secondary,
         borderColor: "black",
         borderWidth: 5,
+    },
+    howToPlay: {
+        backgroundColor: colors.Tertiary,
+        borderRadius: 15,
+        padding: 10,
     },
     image: {
         width: "100%",
@@ -105,6 +125,10 @@ const styles = StyleSheet.create({
         color: colors.White,
         fontSize: 30,
         fontWeight: "bold"
+    },
+    topBorder: {
+        borderTopWidth: 2,
+        borderColor: 'black'
     }
 })
 
